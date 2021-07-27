080f43ae9fcfb69ff27b7e13d2cb14250e6dec82
Douglas Moro <douglas.moro@unochapeco.edu.br>
2021-07-27 16:07:32 -0300

    Criação do tiro

--- js/game.js
+++ js/game.js
@@ -1,20 +1,21 @@
 const btnStart = document.querySelector('.iniciar');
 const nave = document.querySelector('.nave');
-const vida = document.querySelector('.barra-vida'); 
+const vida = document.querySelector('.barra-vida');
+const alien = document.querySelector('.alien');
 const tamanhoTelaHor = window.innerWidth; //Recebe a largura da tela
 const tamanhoTelaVert = window.innerHeight; //Recebe a altura da tela
 
-let posicaoNaveTop = tamanhoTelaVert/2; //Inicializa no meio da tela
-let posicaoNaveLeft = 10; 
-let frame = false;  
+let posicaoNaveTop = tamanhoTelaVert / 2; //Inicializa no meio da tela
+let posicaoNaveLeft = 10;
+let frame = false;
 
 const iniciarJogo = () => {
-    nave.style.display = 'flex'; 
-    btnStart.style.display = 'none'; 
-    vida.style.display = 'flex'; 
+    nave.style.display = 'flex';
+    btnStart.style.display = 'none';
+    vida.style.display = 'flex';
+    alien.style.display = 'none';
     //Inicializa a função que movimenta a nave.
-    document.addEventListener('keydown', moveNave); 
-    document.addEventListener('keydown', tiro); 
+    document.addEventListener('keydown',acaoNave);
 }
 
 btnStart.addEventListener('click', iniciarJogo);
--- js/movendoNave.js
+++ js/movendoNave.js
@@ -1,18 +1,18 @@
-const moveNave = event => {
+const acaoNave = event => {
     let tecla = event.keyCode;
     //37-esquerda 38-cima 39-direita 40-baixo espaço-32
     if (tecla === 37) {
         if (posicaoNaveLeft <= 4) {
             posicaoNaveLeft -= 0;
         } else {
-            posicaoNaveLeft -= 10; //O 10 é o tamanho do deslocamento/salto;
+            posicaoNaveLeft -= 11; //O 10 é o tamanho do deslocamento/salto;
             nave.style.left = `${posicaoNaveLeft}px`;
         }
     } else if (tecla === 39) {
         if (posicaoNaveLeft >= 690) {
             posicaoNaveLeft += 0;
         } else {
-            posicaoNaveLeft += 10;
+            posicaoNaveLeft += 11;
             nave.style.left = `${posicaoNaveLeft}px`;
         }
     }
@@ -20,21 +20,31 @@ const moveNave = event => {
         if (posicaoNaveTop <= 4) {
             posicaoNaveTop += 0
         } else {
-            posicaoNaveTop -= 10;
+            posicaoNaveTop -= 11;
             nave.style.top = `${posicaoNaveTop}px`;
         }
     } else if (tecla === 40) {
         if (posicaoNaveTop >= 454) {
             posicaoNaveTop += 0;
         } else {
-            posicaoNaveTop += 10;
+            posicaoNaveTop += 11;
             nave.style.top = `${posicaoNaveTop}px`;
         }
+    } if (tecla === 32) {
+        tiro(posicaoNaveTop, posicaoNaveLeft);
     }
 }
 
-const tiro = () => {
-    if (tecla === 32) {
-        //BARRA DE ESPAÇO => ATIRA
-    }
+const tiro = (posicaoTiroY, posicaoTiroX) => {
+    console.log("tiro");
+    let divTiro = document.createElement('div');
+    let atributo1 = document.createAttribute('class');
+    let atributo2 = document.createAttribute('style');
+    atributo1.value = 'tiro-nave';
+    atributo2.value = 'top:' + posicaoTiroY + 'px'; 'left:' + posicaoTiroX + 'px';
+    divTiro.setAttributeNode(atributo1);
+    divTiro.setAttributeNode(atributo2);
+
+    document.body.appendChild(divTiro); //Adiciona a div na tela
+
 }
